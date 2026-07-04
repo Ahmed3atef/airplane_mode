8e04c6f149844d35139c22336b7b21151c5b3a09
Ahmed Atef <ahmed.atef@fabrica.com.eg>
2026-07-04 21:36:57 +0000

    feat: Enhance Airplane Ticket and Flight Passenger functionalities with add-ons and full name generation

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, ahmed.atef and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+    refresh(frm) {
 
-// 	},
-// });
+	},
+});
+
+frappe.listview_settings["Airplane Ticket"] = {
+    has_indicator_for_draft: true,
+    has_indicator_for_submit: true,
+    has_indicator_for_cancelled: true,
+};
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,44 @@
 # Copyright (c) 2026, ahmed.atef and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
-	pass
+    
+    def validate(self):
+       
+        validate_add_ons(self.get("add_ons"))
+        
+	
+ 
+    def before_save(self):
+        total_addons = 0
+        for i in self.add_ons:
+            total_addons += i.amount
+        self.total_amount = self.flight_price + total_addons
+        
+    def before_submit(self):
+        validate_status_on_submit(self.status)
+        
+    def on_submit(self):
+        self.status = "Checked-In"
+    
+    def on_cancel(self):
+        self.status = "Boarded"
+
+
+def validate_status_on_submit(status):
+    if status != "Boarded":
+        frappe.throw("Status must be Boarded to submit ")
+
+def validate_add_ons(itmes):
+    add_ons = set()
+    
+    for row in itmes:
+        if row.item in add_ons:
+            frappe.throw(
+                (f"Row #{row.idx}: Add On {frappe.bold(row.item)} already exists")
+            )
+        add_ons.add(row.item)
