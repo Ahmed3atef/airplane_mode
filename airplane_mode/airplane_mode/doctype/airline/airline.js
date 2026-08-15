00aed5af5d871c011ec7390d1ec6cd1c9db26d3d
Ahmed@Atef <clay674302@gmail.com>
2026-08-15 07:21:04 +0000

    refactor: Clean up airline.js and airline.json formatting and remove unused properties

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -3,8 +3,8 @@
 
 frappe.ui.form.on("Airline", {
 	refresh(frm) {
-        if(frm.doc.website){
-            frm.add_web_link(frm.doc.website, __("Visit Website"));
-        }
+		if (frm.doc.website) {
+			frm.add_web_link(frm.doc.website, __("Visit Website"));
+		}
 	},
 });
